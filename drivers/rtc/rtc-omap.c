33ab3af929a0a50b6b911f0d54e68f7bf77512af
Hebbar Gururaja <gururaja.hebbar@ti.com>
2013-03-21 09:35:07 +0530

    rtc: omap: convert to use devm_* API
    rtc-omap driver is modified to use devm_request_and_ioremap() (combining
    request_mem_region and ioremap) and devm_request_irq() APIs.  Since this
    removes the necessity of freeing the related resources the return path
    is also simplified.
    
    Also, replace pr_debug with dev_err in error path since its appropriate
    to display error message immediately.
    
    Signed-off-by: Hebbar Gururaja <gururaja.hebbar@ti.com>

--- drivers/rtc/rtc-omap.c
+++ drivers/rtc/rtc-omap.c
@@ -324,11 +324,12 @@ MODULE_DEVICE_TABLE(of, omap_rtc_of_match);
 
 static int __init omap_rtc_probe(struct platform_device *pdev)
 {
-	struct resource		*res, *mem;
+	struct resource		*res;
 	struct rtc_device	*rtc;
 	u8			reg, new_ctrl;
 	const struct platform_device_id *id_entry;
 	const struct of_device_id *of_id;
+	int ret;
 
 	of_id = of_match_device(omap_rtc_of_match, &pdev->dev);
 	if (of_id)
@@ -352,17 +353,10 @@ static int __init omap_rtc_probe(struct platform_device *pdev)
 		return -ENOENT;
 	}
 
-	mem = request_mem_region(res->start, resource_size(res), pdev->name);
-	if (!mem) {
-		pr_debug("%s: RTC registers at %08x are not free\n",
-			pdev->name, res->start);
-		return -EBUSY;
-	}
-
-	rtc_base = ioremap(res->start, resource_size(res));
-	if (!rtc_base) {
-		pr_debug("%s: RTC registers can't be mapped\n", pdev->name);
-		goto fail;
+	rtc_base = devm_request_and_ioremap(&pdev->dev, res);
+	if (IS_ERR(rtc_base)) {
+		dev_err(&pdev->dev, "failed to remap io region\n");
+		return -ENOMEM;
 	}
 
 	/* Enable the clock/module so that we can access the registers */
@@ -383,7 +377,6 @@ static int __init omap_rtc_probe(struct platform_device *pdev)
 		goto fail0;
 	}
 	platform_set_drvdata(pdev, rtc);
-	dev_set_drvdata(&rtc->dev, mem);
 
 	/* clear pending irqs, and set 1/second periodic,
 	 * which we'll use instead of update irqs
@@ -401,18 +394,21 @@ static int __init omap_rtc_probe(struct platform_device *pdev)
 		rtc_write(OMAP_RTC_STATUS_ALARM, OMAP_RTC_STATUS_REG);
 
 	/* handle periodic and alarm irqs */
-	if (request_irq(omap_rtc_timer, rtc_irq, 0,
-			dev_name(&rtc->dev), rtc)) {
-		pr_debug("%s: RTC timer interrupt IRQ%d already claimed\n",
-			pdev->name, omap_rtc_timer);
+	ret = devm_request_irq(&pdev->dev, omap_rtc_timer, rtc_irq, 0,
+						dev_name(&rtc->dev), rtc);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to request timer irq %d: %d\n",
+							omap_rtc_timer, ret);
 		goto fail1;
 	}
-	if ((omap_rtc_timer != omap_rtc_alarm) &&
-		(request_irq(omap_rtc_alarm, rtc_irq, 0,
-			dev_name(&rtc->dev), rtc))) {
-		pr_debug("%s: RTC alarm interrupt IRQ%d already claimed\n",
-			pdev->name, omap_rtc_alarm);
-		goto fail2;
+	if (omap_rtc_timer != omap_rtc_alarm) {
+		ret = devm_request_irq(&pdev->dev, omap_rtc_alarm, rtc_irq, 0,
+						dev_name(&rtc->dev), rtc);
+		if (ret) {
+			dev_err(&pdev->dev, "Err requesting alarm irq %d: %d\n",
+							omap_rtc_alarm, ret);
+			goto fail1;
+		}
 	}
 
 	/* On boards with split power, RTC_ON_NOFF won't reset the RTC */
@@ -446,8 +442,6 @@ static int __init omap_rtc_probe(struct platform_device *pdev)
 
 	return 0;
 
-fail2:
-	free_irq(omap_rtc_timer, rtc);
 fail1:
 	rtc_device_unregister(rtc);
 fail0:
@@ -455,16 +449,12 @@ fail0:
 		rtc_writel(0, OMAP_RTC_KICK0_REG);
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
-	iounmap(rtc_base);
-fail:
-	release_mem_region(mem->start, resource_size(mem));
 	return -EIO;
 }
 
 static int __exit omap_rtc_remove(struct platform_device *pdev)
 {
 	struct rtc_device	*rtc = platform_get_drvdata(pdev);
-	struct resource		*mem = dev_get_drvdata(&rtc->dev);
 	const struct platform_device_id *id_entry =
 				platform_get_device_id(pdev);
 
@@ -472,11 +462,6 @@ static int __exit omap_rtc_remove(struct platform_device *pdev)
 
 	/* leave rtc running, but disable irqs */
 	rtc_write(0, OMAP_RTC_INTERRUPTS_REG);
-
-	free_irq(omap_rtc_timer, rtc);
-
-	if (omap_rtc_timer != omap_rtc_alarm)
-		free_irq(omap_rtc_alarm, rtc);
 
 	rtc_device_unregister(rtc);
 	if (id_entry && (id_entry->driver_data & OMAP_RTC_HAS_KICKER))
@@ -486,8 +471,6 @@ static int __exit omap_rtc_remove(struct platform_device *pdev)
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
-	iounmap(rtc_base);
-	release_mem_region(mem->start, resource_size(mem));
 	return 0;
 }
 
